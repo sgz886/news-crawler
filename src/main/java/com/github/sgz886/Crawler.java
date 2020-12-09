c891b08ce6edc025cc9c3b78893942e531cd0c1b
Su <sgz886@gmail.com>
2020-12-09 13:16:27 +0800

    实现了爬虫功能
    待处理网页用数据库和list共同存储
    已处理网页用数据库存储
    
    TODO
    1.待处理网页只用数据库存储
    2.单线程改为多线程

--- src/main/java/com/github/sgz886/Crawler.java
+++ src/main/java/com/github/sgz886/Crawler.java
@@ -1,5 +1,6 @@
 package com.github.sgz886;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,69 +13,106 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Crawler {
-    public static void main(String[] args) {
-        String crawler_URL = "https://sina.cn";
+    private static int processedNumberSession = 0;
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "root";
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws SQLException {
+        String jdbcUrl = "jdbc:h2:file:" + System.getProperty("user.dir") + "/news";
+        Connection connection = DriverManager.getConnection(jdbcUrl, USER_NAME, PASSWORD);
         // 待处理的链接池
-        List<String> linkPool = Stream.of(crawler_URL).collect(Collectors.toCollection(ArrayList::new));
-        // 已处理的链接池
-        Set<String> processedLinks = new HashSet<>();
+        List<String> linkPool = loadUrlsFromDatabase(connection, "select LINK from LINKS_TO_BE_PROCESSED");
+
         int times = 0;
-        while (times < 200) {
+        while (times < 1000) {
             times++;
             if (linkPool.isEmpty()) {
                 break;
             }
+            // 从待处理池捞1个处理
             String link = linkPool.remove(0);
-            if (processedLinks.contains(link)) {
+            insertOrDeleteFromDatabase(connection, "delete from LINKS_TO_BE_PROCESSED where LINK = ?", link);
+
+            if (isAlreadyProcessed(connection, link)) {
                 continue;
             }
-            if (!link.contains("sina.cn")) {
-                // 这是我们不感兴趣的,不处理
-                continue;
-            } else {
-                Document doc = httpGetandParseHtml(link);
-                // 1.将所有发现的sina.cn链接存到链接池
-                doc.select("a").stream().map(tag -> tag.attr("href"))
-                        .forEach(href -> addUrlsToPool(href, linkPool, processedLinks));
-                // 2.将article文章视为新闻页面, 存入数据库,否则do nothing
-                storeIntoDatabaseForNewPage(doc);
-                // 4.收尾
-                processedLinks.add(link);
-            }
+
+            Document doc = httpGetandParseHtml(link);
+
+            parseUrlsFromPageAndStoreToDo(connection, linkPool, doc);
+            // TODO: 2020/12/8
+            processNews(doc);
+
+            insertOrDeleteFromDatabase(connection, "insert into LINKS_ALREADY_PROCESSED  (LINK) VALUES ( ? )", link);
         }
-        System.out.println("已处理 = " + processedLinks.size());
+
         System.out.println("待处理 = " + linkPool.size());
+        System.out.println("本次已处理 = " + processedNumberSession);
+        connection.close();
     }
 
-    private static void addUrlsToPool(String href, List<String> linkPool, Set<String> processedLinks) {
-        if (isInterestingLink(href)) {
-            if (href.startsWith("//")) {
-                href = "https:" + href;
-                System.out.println("        插入https前缀");
-            }
-            if (!processedLinks.contains(href)) {
+    // 1.将所有发现的 新链接 存到待处理数据库
+    private static void parseUrlsFromPageAndStoreToDo(Connection connection, List<String> linkPool, Document doc) throws SQLException {
+        for (Element tag : doc.select("a")) {
+            String href = tag.attr("href");
+            if (isInterestingLink(href)) {
+                if (isAlreadyProcessed(connection, href)) {
+                    continue;
+                }
+                if (href.startsWith("//")) {
+                    href = "https:" + href;
+                }
                 linkPool.add(href);
-                System.out.println("    1.添加:" + href + "进入linkPool");
+                insertOrDeleteFromDatabase(connection, "insert into LINKS_TO_BE_PROCESSED (LINK) values ( ? )", href);
+                System.out.println("    1.添加:" + href + "进入 待处理");
             }
         }
     }
 
-    private static void storeIntoDatabaseForNewPage(Document doc) {
-        // 2.将article文章视为新闻页面, 存入数据库,否则do nothing
-        Elements articleTags = doc.select("article");
-        for (Element articleTag : articleTags) {
-            String text = articleTag.text();
-            // TODO: 2020/12/7 放入数据库
-            System.out.println("    2. TODO  标题:" + text.substring(0, Math.min(text.length(), 30)));
+    // 3.收尾 从待处理数据库删除(可以提前做,跟listPool一起,但是如果异常关闭会丢失一条信息);添加到已处理数据库
+    private static void insertOrDeleteFromDatabase(Connection connection, String sql, String href) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, href);
+            statement.execute();
         }
+    }
+
+    private static boolean isAlreadyProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "select LINK from LINKS_ALREADY_PROCESSED where LINK= ?")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (resultSet!=null) {
+                resultSet.close();
+            }
+        }
+    }
+
+    private static List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
+        List<String> results = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery();) {
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+        }
+        return results;
     }
 
     private static Document httpGetandParseHtml(String link) {
@@ -110,4 +148,29 @@ public class Crawler {
     private static boolean isNotLoginPage(String s) {
         return !s.contains("passport.sina.cn");
     }
+
+    // 2.处理当前新闻页面
+    private static void processNews(Document doc) {
+        Elements articleTags = doc.select("article");
+        for (Element articleTag : articleTags) {
+            String text = articleTag.text();
+            // TODO: 2020/12/7 放入数据库
+            System.out.println("    2. 标题:" + text.substring(0, Math.min(text.length(), 30)));
+        }
+        processedNumberSession++;
+    }
+
+    // private static void addUrlsToPool(Element tag, List<String> linkPool, Set<String> processedLinks) {
+    //     String href = tag.attr("href");
+    //     if (isInterestingLink(href)) {
+    //         if (href.startsWith("//")) {
+    //             href = "https:" + href;
+    //             System.out.println("        插入https前缀");
+    //         }
+    //         if (!processedLinks.contains(href)) {
+    //             linkPool.add(href);
+    //             System.out.println("    1.添加:" + href + "进入 待处理");
+    //         }
+    //     }
+    // }
 }
